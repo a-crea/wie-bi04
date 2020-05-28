df4053fc6dd80fc4b4205101b98b8b6e0b4be8fc
a-crea <58514653+a-crea@users.noreply.github.com>
2020-05-28 18:25:41 +0200

    Room almost completed: added chat, waiting and fixed some pagination issues. Professor page buf fixed. Some style changes.

--- public/js/room.js
+++ public/js/room.js
@@ -2,17 +2,24 @@
 $(function () {
     var db = firebase.firestore();
     var realTimeDb = firebase.database();
+    var name = null;
     var idRoom = null;
+    var peersInfo = {};
     var uid = null;
     var prof = null;
+    var roomWasActivated = false;
+    var remainingTime = null;
+    var endTimeGlobal = null;
+    var countdown = null;
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             uid = user.uid;
             var docRef = db.collection("users").doc(uid);
+            name = user.displayName
             docRef.get().then(function (doc) {
                 if (doc.exists && doc.data().role == "student" && doc.data().professor) {
                     prof = doc.data().professor;
-                    connectToRTDB();
+                    connectToRTDBStud();
                     // $("#professor-name").html(user.displayName);
                     // let alreadyInDb = null;
                     // realTimeDb.ref(prof).once('value', function (snapshot) {
@@ -39,16 +46,85 @@ $(function () {
         }
     });
 
-    function connectToRTDB() {
+    document.getElementById("btn-send").addEventListener("click", sendMessage);
+    document.getElementById("request-time-btn").addEventListener("click", requireMoreTime);
+
+    function connectToRTDBStud() {
         var studentRealTime = realTimeDb.ref(prof + '/students/' + uid);
         studentRealTime.on('value', function (snapshot) {
-            console.log(snapshot)
-            console.log(snapshot.val())
-            if (snapshot.val().room){
-                connectToRoom(snapshot.val().room);
+            if (snapshot.val().hasOwnProperty('room') && idRoom == null){
+                $("#room-waiting").css("display","none");
+                $(".room-active").css("display","flex");
+                idRoom = snapshot.val().room
+                connectToRoom();
+                connectToRTDBRoom();
+                roomWasActivated = true;
+            } else if (!snapshot.val().hasOwnProperty('room') && roomWasActivated === true) {
+                location.reload();
             }
         });
     }
+
+    function connectToRTDBRoom(){
+        var endTimeRT = realTimeDb.ref(prof + '/rooms/' + idRoom + '/endtime');
+        endTimeRT.on('value', function (snapshot) {
+            endTimeGlobal = new Date(snapshot.val());
+            countdownInit()
+        });
+        var moreTimeRT = realTimeDb.ref(prof + '/rooms/' + idRoom + '/moreTime');
+        moreTimeRT.on('value', function (snapshot) {
+            if(snapshot.val()>0){
+                $("#more-time-select").attr("disabled", true);
+                $("#request-time-btn").attr("disabled", true);
+            } else {
+                $("#more-time-select").removeAttr("disabled");
+                $("#request-time-btn").removeAttr("disabled");
+            }
+            // console.log("time, disable button if >0")
+        });
+        var helpRT = realTimeDb.ref(prof + '/rooms/' + idRoom + '/needsHelp');
+        helpRT.on('value', function (snapshot) {
+            console.log("help, disable button if true")
+        });
+    }
+
+    function requireMoreTime(){
+        let timeRequested = $("#more-time-select").val();
+        timeRequested = parseInt(timeRequested, 10);
+        console.log(timeRequested)
+        realTimeDb.ref(prof + '/rooms/' + idRoom ).update({
+            moreTime: timeRequested
+        })
+    }
+
+    function countdownInit() {
+        fetch('https://worldtimeapi.org/api/timezone/Europe/Rome')
+            .then(function (response) {
+                return response.text();
+            }).then(function (data) {
+                if (data != 'null') {
+                    $("#countdown-container").css("display", "inline-block");
+                    let time = new Date(JSON.parse(data).utc_datetime);
+                    countdown = setInterval(function () {
+                        remainingTime = Date.parse(endTimeGlobal) - Date.parse(time);
+                        let m = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+                        let s = Math.floor((remainingTime % (1000 * 60)) / 1000);
+                        time.setSeconds(time.getSeconds() + 1);
+                        if (s.toString().length == 1) {
+                            s = "0" + s;
+                        }
+                        $("#time").html(m + ":" + s)
+                        if (remainingTime < 0) {
+                            $("#time").html("FINISHED");
+                            location.reload();
+                        }
+                    }, 1000);
+                }
+            }).catch(function (err) {
+                console.log('ERRORE ', err);
+            })
+    }
+
     function signOut() {
         firebase.auth().signOut().then(function () {
             window.location.replace(url + "/index.html");
@@ -56,12 +132,40 @@ $(function () {
             // An error happened.
         });
     }
-
-    window.enableAdapter = true; // enable adapter.js
-    function connectToRoom(roomId){
+    $(".chat-input").keypress(function (event) {
+        if (event.keyCode === 13 && $(".chat-input").val().length > 0) {
+            sendMessage()
+        }
+    });
+    $(".chat-input").keyup(function (event) {
+        if ($(".chat-input").val().length > 0) {
+            $("#btn-send").removeAttr("disabled");
+        } else {
+            $("#btn-send").attr("disabled", true);
+        }
+    });
+    // $(".chat-input").change(function () {
+    //     if ($(".chat-input").val().length > 0){
+    //         $("#btn-send").removeAttr("disabled");
+    //     } else {
+    //         $("#btn-send").attr("disabled", true);
+    //     }
+    // });
+    function sendMessage(){
+        let msg = $("input").val();
+        $("input").val("");
+        $(".chat-container").append('<div class="message-container me"><div class="chat-message-me"><span>'+name+'</span>'+msg+'</div></div>')
+        connection.send(msg)
+    }
+    function isWaiting(){
+        if (Object.keys(peersInfo).length == 0) {
+            $("#video-placeholder").css("display", "flex");
+        }
+    }
+    window.enableAdapter = true;
+    function connectToRoom(){
         isRoomExists = false;
-        let roomid=roomId
-        connection.openOrJoin(roomid, function (isRoomExists, roomid) {
+        connection.openOrJoin(idRoom, function (isRoomExists, roomid) {
             if (!isRoomExists) {
                 // showRoomURL(roomid);
             }
@@ -105,25 +209,67 @@ $(function () {
         ]
     }];
 
+    connection.onopen = function (event) {
+        let remoteUserId = event.userid;
+        peersInfo[remoteUserId] = {}
+        connection.send(name);
+    };
+
+    connection.onmessage = function (event) {
+        let remoteUserId = event.userid;
+        if (peersInfo[remoteUserId].hasOwnProperty('name')) {
+            // do something
+            let msg = event.data;
+            $(".chat-container").append('<div class="message-container"><div class="chat-message"><span>' + peersInfo[remoteUserId].name + '</span>' + msg + '</div></div>')
+        } else {
+            let remoteName = event.data;
+            peersInfo[remoteUserId] = {
+                name: remoteName
+            }
+        }
+    };
+    connection.onleave = function (event) {
+        let remoteUserId = event.userid;
+        $('#' + remoteUserId).remove();
+        delete peersInfo[remoteUserId];
+        isWaiting();
+        //check how many divs in container, if 0 show video placeholder
+    };
+    connection.onclose = function (event) {
+        let remoteUserId = event.userid;
+        $('#' + remoteUserId).remove();
+        delete peersInfo[remoteUserId];
+        isWaiting();
+    };
     connection.onstream = function (event) {
+        console.log(peersInfo);
+        let remoteUserId = event.userid;
         let classVideo, classVideoContainer;
-        var video = document.createElement('video');
-        var div = document.createElement('div');
+        let video = document.createElement('video');
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        let nameSpan;
         event.mediaElement.removeAttribute('src');
         event.mediaElement.removeAttribute('srcObject');
         if (event.type === 'local') {
             connection.videosContainer = document.getElementById('video-me-ale');
+            video.volume = 0;
             video.muted = true;
             classVideo = 'video-local';
             classVideoContainer = 'video-local-container';
         } else {
+            $("#video-placeholder").css("display","none");
             connection.videosContainer = document.getElementById('videos-remote-container');
             classVideo = 'video-remote';
             classVideoContainer = 'video-remote-container';
+            nameSpan = document.createTextNode(peersInfo[remoteUserId].name);
+            span.appendChild(nameSpan);
+            div.append(span);
         }
+        span.classList.add('name-in-video');
         video.controls = true;
         video.classList.add(classVideo);
-        div.setAttribute('id', uid);
+        div.setAttribute('id', remoteUserId);
         div.classList.add(classVideoContainer);
         video.srcObject = event.stream;
         div.append(video);
--- public/js/teaching.js
+++ public/js/teaching.js
@@ -1,4 +1,4 @@
-/*
+
 var db = firebase.firestore();
 var profUid;
 var profUidSelected = null;
@@ -214,4 +214,3 @@ function launchModal(label, body) {
     $('#modal-body').html(body);
     $('#modal-message').modal('toggle');
 } 
-*/
